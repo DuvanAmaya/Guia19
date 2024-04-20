bbd94a9bd0fb96a6060b0d5cd4f3709fd12223f1
Duvan Camilo Amaya Lopez <duvan.amayalopez@unicolombo.edu.co>
2024-04-20 13:34:27 -0500

    Guia19

---
+++ src/guia19/Guia19.java
@@ -0,0 +1,11 @@
+package guia19;
+
+import VentanaGuia.VentanaPrincipal;
+
+public class Guia19 {
+    public static void main(String[] args) {
+        VentanaPrincipal ventana = new VentanaPrincipal();
+        ventana.setLocationRelativeTo(ventana);
+        ventana.setVisible(true);
+    }
+}
